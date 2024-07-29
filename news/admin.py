a8d4512fd4726b79135d84f9b4634a099285c471
luo chengbo <2879330978@qq.com>
2024-07-29 10:40:31 +0800

    设置News和Category模型后台管理、后台管理新建一些数据

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from news.models import Category, News
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
