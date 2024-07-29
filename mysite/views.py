71aaa54fa09dda41a130419a32a6861da5a5addd
luo chengbo <2879330978@qq.com>
2024-07-29 09:54:29 +0800

    团队头像上传、创建News和Category模型

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from slide.models import Slide
+from team.models import Team
 # Create your views here.
 def index(request):
+    # 获取slide表数据
     slides = Slide.objects.all()
-    return render(request, 'index.html',{'slides':slides})
+    # 获取团队成员
+    team = Team.objects.all().order_by('-rank')
+    return render(request, 'index.html',{
+        'slides': slides,
+        'team': team,
+    })
--- team/models.py
+++ team/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Team(models.Model):
     id = models.AutoField(primary_key=True)
-    avatar = models.ImageField('头像',upload_to='slide',help_text='最佳尺寸：500x755')
+    avatar = models.ImageField('头像',upload_to='item',help_text='最佳尺寸：500x755')
     name = models.CharField('姓名', max_length=100)
     title = models.CharField('职务', max_length=100)
     rank = models.IntegerField('排序')
