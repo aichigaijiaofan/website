518b42bc15b6d13a3a2e447d994becd2ec845d3a
luo chengbo <2879330978@qq.com>
2024-07-29 11:03:54 +0800

    创建新闻列表页和新闻详情页并配置相应路由

---
+++ news/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = {
+    path('list/', views.new_list),
+    path('<int:id>/', views.detail)
+}
--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from news.models import News
 # Create your views here.
+def new_list(request):
+    news_list = News.objects.all()
+    return render(request, 'list.html',{'news_list': news_list})
+
+
+def detail(request, id):
+    news = News.objects.get(pk=id)
+    return render(request, 'detail.html', {'news': news})
