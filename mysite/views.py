ad165a38105268a74d37622215738d4927d856a5
luo chengbo <2879330978@qq.com>
2024-07-30 14:03:50 +0800

    展示首页成功案例分类数据

--- mysite/views.py
+++ mysite/views.py
@@ -11,8 +11,11 @@ def index(request):
     team = Team.objects.all().order_by('-rank')
     # 获取资讯中心新闻数据
     news = News.objects.filter(category_id=1).order_by('-created_at')[:3]
+    # 获取成功案例内容
+    cases = News.objects.filter().exclude(category_id=1).order_by('-created_at')
     return render(request, 'index.html',{
         'slides': slides,
         'team': team,
         'news': news,
+        'cases': cases,
     })
